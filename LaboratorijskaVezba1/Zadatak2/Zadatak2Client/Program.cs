9130b0d4f326b2b854dd2f2d1fa51b6af420e4a3
ChiChaSsSSs <aantic112@gmail.com>
2026-04-01 11:21:46 +0200

    Add files via upload

---
+++ LaboratorijskaVezba1/Zadatak2/Zadatak2Client/Program.cs
@@ -0,0 +1,82 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Zad2;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7080");
+var client = new NumberService.NumberServiceClient(channel);
+string? input;
+
+try
+{
+    do
+    {
+        Console.WriteLine("Perform procedure AverageValue. (Press 1)");
+        Console.WriteLine("Perform procedure ComputeArray. (Press 2)");
+        Console.WriteLine("Exit. (Press 0)");
+        input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "0": Console.WriteLine("Exiting..."); break;
+            case "1": await AverageValue(); break;
+            case "2": await ComputeArray(); break;
+            default: Console.WriteLine("Invalid input. Please try again."); break;
+        }
+    } while (input != "0");
+}
+catch (RpcException ex)
+{
+    Console.WriteLine($"RPC Error: {ex.Status.Detail}");
+}
+async Task AverageValue()
+{
+    Console.WriteLine("Enter the border number for average procedure:");
+    int border = int.Parse(Console.ReadLine());
+
+    try
+    {
+        var request = new Request { Value = border };
+        var response = await client.AverageValueAsync(request);
+        Console.WriteLine(response.ToString());
+    }
+    catch (RpcException ex)
+    {
+        Console.WriteLine($"RPC Error: {ex.Status.Detail}");
+    }
+}
+async Task ComputeArray()
+{
+    Console.WriteLine("Enter the umber of elements in the array:");
+    int n = int.Parse(Console.ReadLine());
+    List<int> values = new List<int>();
+    Console.WriteLine("Enter the elements of the array:");
+    for (int i = 0; i < n; i++)
+    {
+        values.Add(int.Parse(Console.ReadLine()));
+    }
+
+    try
+    {
+        var call = client.ComputeArray();
+
+        var readTask = Task.Run(async () =>
+        {
+            await foreach (var response in call.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine(response.Value);
+            }
+        });
+
+        foreach (var v in values)
+        {
+            await call.RequestStream.WriteAsync(new Request { Value = v});
+        }
+
+        await call.RequestStream.CompleteAsync();
+        await readTask;
+    }
+    catch (RpcException ex)
+    {
+        Console.WriteLine($"RPC Error: {ex.Status.Detail}");
+    }
+}
---
+++ LaboratorijskaVezba1/Zadatak2/Zadatak2Server/Services/NumberServiceImpl.cs
@@ -0,0 +1,46 @@
+﻿using Grpc.Core;
+using Google.Protobuf.WellKnownTypes;
+using Zad2;
+
+namespace Zadatak2Server.Services
+{
+    public class NumberServiceImpl : NumberService.NumberServiceBase
+    {
+        private static float acc = 1;
+
+        public override Task<Empty> AverageValue(Request request, ServerCallContext context)
+        {
+            if (request.Value <= 0)
+            {
+                return Task.FromResult(new Empty());
+            }
+
+            float sum = 0;
+            for (int i =1; i <= request.Value; i++)
+            {
+                sum += i;
+            }
+
+            acc += sum / (float)request.Value;
+
+            return Task.FromResult(new Empty());
+        }
+
+        public override async Task ComputeArray(IAsyncStreamReader<Request> request, IServerStreamWriter<Reply> response, ServerCallContext context)
+        {
+            int counter = 1;
+            await foreach (var req in request.ReadAllAsync())
+            {
+                if (counter % 3 == 0)
+                {
+                    await response.WriteAsync(new Reply { Value = req.Value * acc });
+                }
+                else
+                {
+                    await response.WriteAsync(new Reply { Value = req.Value - acc });
+                }
+                counter++;
+            }
+        }
+    }
+}
